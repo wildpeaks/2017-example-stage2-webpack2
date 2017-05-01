6d395567055927533e2da78325d51882e95fef24
Cecile Muller <git@wildpeaks.fr>
2017-05-01 04:32:19 +0200

    Added Karma config (for mocha tests, and coverage)
    After **a lot** of testing multiple solutions, this
    `istanbul-instrumenter-loader` based version has almost all my
    requirements. The main thing missing is that includeAllSources doesn't
    actually include all sources, so modules that don't have a .tests.js
    file aren't shown as not covered :( This was also an issue back with
    `nyc` when I had to transpile, so it's not really a new issue, but at
    least with nyc it was fixed eventually, so it sucks that this issue is
    back.

---
+++ karma.conf.js
@@ -0,0 +1,56 @@
+/* eslint-env node */
+const path = require('path');
+const getWebpackConfig = require('./webpack.config.js');
+
+module.exports = config => {
+	const coverage = (config.coverage === true);
+	const webpackConfig = getWebpackConfig();
+	if (coverage){
+		webpackConfig.module.rules.unshift({
+			test: /\.js$/,
+			exclude: /tests/,
+			include: [
+				path.join(__dirname, 'src')
+			],
+			use: {
+				loader: 'istanbul-instrumenter-loader',
+				options: {
+					produceSourceMap: true,
+					esModules: true
+				}
+			}
+		});
+	}
+	config.set({
+		browsers: [/*'IE', */'Chrome'/*, 'Firefox', 'PhantomJS'*/],
+		frameworks: ['mocha'],
+		client: {
+			mocha: {
+				reporter: 'html'
+			}
+		},
+		files: [
+			{pattern: 'src/**/*.tests.js'}
+		],
+		preprocessors: {
+			'src/**/*.tests.js': ['webpack', 'sourcemap']
+		},
+		webpack: webpackConfig,
+		webpackMiddleware: webpackConfig.devServer,
+		reporters: ['progress', coverage ? 'coverage-istanbul' : 'html'],
+		htmlReporter: {
+			outputFile: 'www/karma.tests.html',
+			pageTitle: 'Karma Tests',
+			subPageTitle: 'All tests that were run in Karma',
+			groupSuites: true,
+			useCompactStyle: true,
+			useLegacyStyle: true
+		},
+		coverageIstanbulReporter: {
+			reports: ['html', 'lcovonly', 'text-summary'],
+			dir: path.join(__dirname, 'www/coverage/%browser%'),
+			includeAllSources: true,
+			skipFilesWithNoCoverage: false
+		}
+	});
+};
